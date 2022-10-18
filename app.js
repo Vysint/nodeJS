dc03253ca9e9448f18ddbf8c1e914d9bfa1d4abf
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-18 21:16:58 +0300

    Added res for sending responses in the node server

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+  res.end();
 })
 
 server.listen(3000);
