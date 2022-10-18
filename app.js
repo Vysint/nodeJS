f4222e796ec4d82c14d602959bed4eccf8d9fb40
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-18 21:32:25 +0300

    Added requests routing

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const http = require('http');
+const fs = require('fs');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<header><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action = "/message" method = "POST"><input type = "text" name = "message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+  res.write('<body><h1>Hello from my node.js Server!</h1></body>');
   res.end();
-})
+});
 
 server.listen(3000);
