a5547e2b4530ec4fcb7caab725772f76386e7582
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-19 17:31:27 +0300

    Returned a 404 page

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 const express = require("express");
 
+const path = require("path");
+
 const app = express();
 
-const adminRoutes = require('./routes/admin'); 
+const adminRoutes = require("./routes/admin");
 
-const shopRoutes = require('./routes/shop');
+const shopRoutes = require("./routes/shop");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-app.use('/admin',adminRoutes);
+app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found!</h1>')
-})
-
-
-
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(3000);
