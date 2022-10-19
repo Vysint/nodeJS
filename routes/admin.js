19d251dd3d9ca5fc1103fd5b3ae6d4e8b2d43819
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-19 17:41:48 +0300

    Added a helper function for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
+const rootDir = require('../util/path');
+
 const path = require("path");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,13 @@ const expresss = require("express");
 
 const path = require('path');
 
+const rootDir = require('../util/path');
+
 const router = expresss.Router();
 
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../','views', 'shop.html'));
+  res.sendFile(path.join(rootDir,'views', 'shop.html'));
 });
 
 
