9a237880cce8720a9d8cb2a56cfb4ce2837131ec
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-19 21:22:47 +0300

    Served static files i.e css and images etc

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require("express");
-
 const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 const adminRoutes = require("./routes/admin");
-
 const shopRoutes = require("./routes/shop");
 
-const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/admin", adminRoutes);
 
