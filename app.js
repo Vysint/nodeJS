fc0a131172af80d872047a90befc2477c14b327b
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-19 16:16:32 +0300

    Added Express Router

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const express = require("express");
-const bodyParser = require('body-parser');
 
 const app = express();
+
+const adminRoutes = require('./routes/admin'); 
+
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-app.use("/add-product", (req, res, next) => {
-  res.send('<form action="/product" method = "POST"><input type="text" name ="title"><button type="submit">Add Product</button></form>');
-});
+app.use(adminRoutes);
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body)
-  res.redirect('/');
-})
+app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express.js!</h1>");
-});
+
 
 
 app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,12 @@
+const expresss = require("express");
+
+const router = expresss.Router();
+
+
+router.get("/", (req, res, next) => {
+  res.send("<h1>Hello from Express.js!</h1>");
+});
+
+
+
+module.exports = router;
