d83e0bf7f4fcb0e441a51cad876b332293847719
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-21 23:30:51 +0300

    Fetched data from files via models

--- models/product.js
+++ models/product.js
@@ -1,13 +1,38 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
   save() {
-    products.push(this);
+    const path1 = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(path1, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(path1, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const path1 = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(path1, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 };
