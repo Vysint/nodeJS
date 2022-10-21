06279785a3556addbe785fbba1dc4f1338d13645
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-21 16:03:48 +0300

    Pug Finished

--- routes/shop.js
+++ routes/shop.js
@@ -1,18 +1,15 @@
-const expresss = require("express");
-
 const path = require('path');
+
+const express = require('express');
 
 const rootDir = require('../util/path');
 const adminData = require('./admin');
 
-const router = expresss.Router();
+const router = express.Router();
 
-
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products});
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
-
-
 
 module.exports = router;
