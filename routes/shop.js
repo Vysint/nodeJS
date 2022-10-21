4158330bcec405b71e489595a22066e6088f7cf1
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-21 15:47:04 +0300

    Outputted Dynamic content

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,8 @@ const router = expresss.Router();
 
 
 router.get("/", (req, res, next) => {
-  res.render('shop');
+  const products = adminData.products;
+  res.render('shop', {prods: products});
 });
 
 
