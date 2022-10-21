9f8942480fb456cf4cf6b092e9c1d8f205b633f8
Vincent Kipngetich <vinniebluz@icloud.com>
2022-10-21 13:44:53 +0300

    Shared Data across Requests and Users

--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,13 @@ const expresss = require("express");
 const path = require('path');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = expresss.Router();
 
 
 router.get("/", (req, res, next) => {
+  console.log('shop.js', adminData.products);
   res.sendFile(path.join(rootDir,'views', 'shop.html'));
 });
 
