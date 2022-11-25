0e06600155522712712d7427918ab3c0aa3f0d17
Vincent Kipngetich <vinniebluz@icloud.com>
2022-11-25 17:46:22 +0300

    Added deleting cart item functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -66,18 +66,21 @@ exports.postCart = (req, res, next) => {
     })
     .then((result) => {
       console.log(result);
-      res.redirect('/cart')
+      res.redirect("/cart");
     });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect("/cart");
-  });
+  req.user
+    .deleteItemFromCart(prodId)
+    .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
-
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     path: "/orders",
