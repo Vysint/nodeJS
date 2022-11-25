a3350b949189dc7bcb51c75657ecb5a2cbb3e95e
Vincent Kipngetich <vinniebluz@icloud.com>
2022-11-25 16:12:40 +0300

    Added the 'Add to Cart' functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -66,10 +66,13 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect("/cart");
+  Product.findByid(prodId)
+    .then((product) => {
+      return req.user.addToCart(product);
+    })
+    .then((result) => {
+      console.log(result);
+    });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
--- models/user.js
+++ models/user.js
@@ -4,19 +4,35 @@ const ObjectId = mongodb.ObjectId;
 const getDb = require("../util/database").getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
   }
+  addToCart(product) {
+    const updatedCart = {
+      items: [{ productId: new ObjectId(product._id), quantity: 1 }],
+    };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
 
   static findById(userId) {
     const db = getDb();
-    return db.collection("users").find({ _id: new ObjectId(userId) })
-    .next()
+    return db
+      .collection("users")
+      .find({ _id: new ObjectId(userId) })
+      .next();
   }
 }
 
