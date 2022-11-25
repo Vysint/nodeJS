1f8d34a4a88f243a334c3fd9b256142f0933d03c
Vincent Kipngetich <vinniebluz@icloud.com>
2022-11-25 12:16:02 +0300

    Fetched a single Product from the database

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,15 +17,18 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-    res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.title,
-      path: "/products",
+  Product.findByid(prodId)
+    .then((product) => {
+      res.render("shop/product-detail", {
+        product: product,
+        pageTitle: product.title,
+        path: "/products",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  });
 };
-
 exports.getIndex = (req, res, next) => {
   Product.fetchAll()
     .then((products) => {
