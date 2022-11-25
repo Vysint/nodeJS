4317da77bcd144784e74b5896b49decee7a31ad6
Vincent Kipngetich <vinniebluz@icloud.com>
2022-11-25 18:26:03 +0300

    Added Orders retrieval

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require("../models/product");
-const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll()
@@ -80,6 +79,14 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .catch((err) => {
       console.log(err);
     });
+};
+exports.postOrder = (req, res, next) => {
+  req.user
+    .addOrder()
+    .then((result) => {
+      res.redirect("/orders");
+    })
+    .catch((err) => console.log(err));
 };
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
