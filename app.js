d2008a9fa8dac87eaf0310e6f1806a1de9f34e70
Vincent Kipngetich <vinniebluz@icloud.com>
2022-11-26 12:37:13 +0300

    Saved data to the mongodb database throught mongoose

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const User = require("./models/user");
+// const User = require("./models/user");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -18,14 +18,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("6381d4701b276e721499b5e4")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("6381d4701b276e721499b5e4")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -34,7 +34,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://paige:0723055173@cluster0.myzmae1.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://paige:0723055173@cluster0.myzmae1.mongodb.net/shop?retryWrites=true&w=majority"
   )
   .then((result) => {
     app.listen(3000, () => {
