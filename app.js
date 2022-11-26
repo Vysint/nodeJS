e53be2d99ca7f020d89e18285d9f9b28a183c609
Vincent Kipngetich <vinniebluz@icloud.com>
2022-11-26 12:16:59 +0300

    Added Mongoose for connection to the mongoDB database and define it's data schema

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
 const User = require("./models/user");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById("6380b682b36ff29930fbd600")
+  User.findById("6381d4701b276e721499b5e4")
     .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
@@ -32,6 +32,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    "mongodb+srv://paige:0723055173@cluster0.myzmae1.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(3000, () => {
+      console.log("Listening at PORT 3000");
+    });
+  })
+  .catch((err) => console.log(err));
