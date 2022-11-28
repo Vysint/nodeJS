fd33f15a5a2f0b9e12390ab90cdc84f90120923f
Vincent Kipngetich <vinniebluz@icloud.com>
2022-11-28 13:12:02 +0300

    Deleted session and cookies

--- app.js
+++ app.js
@@ -38,7 +38,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  User.findById("5bab316ce0a7c75f783cb8a8")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then((user) => {
       req.user = user;
       next();
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,26 @@
-const user = require("../models/user");
+const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-  console.log(req.session.isLoggedIn);
-  res.render("auth/login", {
-    path: "/login",
-    pageTitle: "Login",
-    isAuthenticated: false,
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  user.findById("63847abf65ff167b6f4051ac").then((user) => {
-    req.session.isLoggedIn = true;
-    req.session.user = user;
-    res.redirected("/");
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
   });
-  res.redirect("/");
 };
