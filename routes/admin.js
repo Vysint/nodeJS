e0b4135d8d3625ee154617d49a92fae881268066
Vincent Kipngetich <vinniebluz@icloud.com>
2022-11-30 20:19:56 +0300

    Validated product Editing

--- routes/admin.js
+++ routes/admin.js
@@ -1,44 +1,56 @@
-const express = require("express");
-const { body } = require("express-validator/check");
+const path = require('path');
 
-const adminController = require("../controllers/admin");
-const isAuth = require("../middleware/is-auth");
+const express = require('express');
+const { body } = require('express-validator/check');
+
+const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get("/add-product", isAuth, adminController.getAddProduct);
+router.get('/add-product', isAuth, adminController.getAddProduct);
 
 // /admin/products => GET
-router.get("/products", isAuth, adminController.getProducts);
+router.get('/products', isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
 router.post(
-  "/add-product",
+  '/add-product',
   [
-    body("title").isString().isLength({ min: 5 }).trim(),
-    body("imageUrl").isURL(),
-    body("price").isFloat(),
-    body("description").isLength({ min: 5, max: 200 }).trim(),
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
   ],
   isAuth,
   adminController.postAddProduct
 );
 
-router.get(
-  "/edit-product/:productId",
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+
+router.post(
+  '/edit-product',
   [
-    body("title").isAlphanumeric().isLength({ min: 5 }).trim(),
-    body("imageUrl").isURL(),
-    body("price").isFloat(),
-    body("description").isLength({ min: 5, max: 200 }).trim(),
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('imageUrl').isURL(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
   ],
   isAuth,
-  adminController.getEditProduct
+  adminController.postEditProduct
 );
 
-router.post("/edit-product", isAuth, adminController.postEditProduct);
-
-router.post("/delete-product", isAuth, adminController.postDeleteProduct);
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
