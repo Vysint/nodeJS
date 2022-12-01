f498f3ce07591398312e1e6ba51d0da683520229
Vincent Kipngetich <vinniebluz@icloud.com>
2022-12-02 01:37:51 +0300

    Added DOM Manipulation  to delete a product from the client side

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,11 +1,16 @@
 function deleteProduct(btn) {
   const prodId = btn.parentNode.querySelector("[name=productId]").value;
 
+  const productElement = btn.closest("article");
   fetch("/admin/product/" + prodId, {
     method: "DELETE",
   })
     .then((result) => {
-      console.log(result);
+      return result.json();
+    })
+    .then((data) => {
+      console.log(data);
+      productElement.parentNode.removeChild(productElement);
     })
     .catch((err) => console.log(err));
 }
