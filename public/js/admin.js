3414019dc9afd0fcc2a8ab0cb85b833f7c238535
Vincent Kipngetich <vinniebluz@icloud.com>
2022-12-02 01:32:33 +0300

    Sent and handled background Requests

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,11 @@
 function deleteProduct(btn) {
-  const prodId = btn.parentNode.querySelector('[name=productId]').value;
+  const prodId = btn.parentNode.querySelector("[name=productId]").value;
+
+  fetch("/admin/product/" + prodId, {
+    method: "DELETE",
+  })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => console.log(err));
 }
